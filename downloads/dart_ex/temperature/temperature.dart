e4aa0bb5d957fe47f5faca7848ee4e7fa21bff0d
cheng-shiou <55220871+40823131@users.noreply.github.com>
2019-11-16 17:43:44 +0800

    Create temperature.dart

---
+++ downloads/dart_ex/temperature/temperature.dart
@@ -0,0 +1,17 @@
+int x = 0;
+num F = 86;
+num C = 30;
+
+void main(){
+  if (x == 0) {
+  num F2 = C*9/5+32;
+			print('F=$F2');
+  }
+  else{
+    
+    num C2 = (F-32)*5/9;
+			print('C=$C2');
+  }
+  print('x=0,1切換(華氏、攝氏)');
+  
+}
