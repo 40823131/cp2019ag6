d8c52857e486eba2f07185e22c8490de9d0d50c1
cheng-shiou <55220871+40823131@users.noreply.github.com>
2019-12-04 23:22:39 +0800

    Update temperature.dart

--- downloads/dart_ex/temperature/temperature.dart
+++ downloads/dart_ex/temperature/temperature.dart
@@ -1,17 +1,18 @@
-int x = 0;
-num F = 86;
-num C = 30;
-
-void main(){
-  if (x == 0) {
-  num F2 = C*9/5+32;
-			print('F=$F2');
-  }
-  else{
-    
-    num C2 = (F-32)*5/9;
-			print('C=$C2');
-  }
-  print('x=0,1切換(華氏、攝氏)');
+main() {
+  List temp = ["20C", "30C", "50F", "40C", "23F"];
+  for (var i in temp) {
   
-}
+    if (i[2] == "C") {
+    var a = (i[0]+i[1]);
+     var c = int.parse (a);
+    double f =c*9/5+32;
+    print ("攝氏 $c 度=華氏 $f 度");
+    }
+    else{
+      var a = (i[0]+i[1]);
+     var f = int.parse (a);
+    double c =(f-32)*5/9;
+    print ("華氏 $f 度=攝氏 $c 度 ");
+    }
+  } 
+} 
